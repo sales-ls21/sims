0c92f048e05a3778f973ab4d107d3c11819db833
LaDonna <boxgirl85@gmail.com>
2017-04-09 14:27:28 -0500

    angular templating functioning

--- app/controllers/dashboardCtrl.js
+++ app/controllers/dashboardCtrl.js
@@ -2,39 +2,42 @@
 
 app.controller("dashboardCtrl", function($scope, $location, dataFactory){
 
-	$scope.department = [];
 	$scope.categories = [];
+	$scope.finalCategories = [];
 	$scope.livingroom = [];
 	$scope.diningroom = [];
 	$scope.bedroom = [];
 	$scope.office = [];
-
-
-
 	dataFactory.getByDepartment("furniture")
 		.then((obj)=>{
 			let data = obj.data;
 			Object.values(data).forEach(function(key){
 				$scope.categories.push(key.category);
 				$scope.categories = [...new Set($scope.categories)];
+				$scope.$apply();
 				if(key.category === "living room"){
 					$scope.livingroom.push(key);
+					$scope.$apply();
 				}else if(key.category === "dining room"){
 					$scope.diningroom.push(key);
+					$scope.$apply();
 				}else if(key.category === "bedroom"){
 					$scope.bedroom.push(key);
+					$scope.$apply();
 				}else if(key.category === "office"){
 					$scope.office.push(key);
+					$scope.$apply();
 				}
-			});
-			console.log($scope.categories);
-			console.log($scope.livingroom);
-			console.log($scope.diningroom);
-			console.log($scope.bedroom);
-			console.log($scope.office);
-			
+			});	
+			$scope.categories.forEach(function(v){
+					$scope.finalCategories.push(v.toUpperCase());
+					$scope.$apply();
+				});		
+	});
 
-		});
+
+
+
 
 	
 });
