591c336d2e03002338535c2261e6a7c1324d3907
LaDonna <boxgirl85@gmail.com>
2017-04-10 00:24:01 -0500

    add product functionality completed

--- app/controllers/addProductCtrl.js
+++ app/controllers/addProductCtrl.js
@@ -1,5 +1,39 @@
 "use strict";
 
-app.controller("addProductCtrl", function(){
-	
+app.controller("addProductCtrl", function(authFactory, $scope, $location, dataFactory){
+	$scope.currentUser = null;
+
+	authFactory.isAuthenticated()
+	.then((obj)=>{
+		$scope.currentUser = authFactory.getUser();
+		$scope.$apply();
+	});
+
+
+	$scope.product = {
+		SKU: "",
+		brand: "",
+		department: "",
+		category: "",
+		type: "",
+		price: "",
+		quantity: "",
+		location: "",
+		description: "",
+		date_created: new Date().toISOString(),
+	};
+
+	$scope.logout = ()=>{
+		authFactory.logoutUser();
+		$location.url("/");
+	};
+
+	$scope.save = ()=>{
+		$scope.product.created_by = $scope.currentUser;
+		dataFactory.addProduct($scope.product)
+		.then((obj)=>{
+			$location.url("/dashboard");
+			$scope.$apply();
+		});
+	};
 });
